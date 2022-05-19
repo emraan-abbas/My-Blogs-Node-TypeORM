932108107a81e7607e13c4847ffd43eef694dc5f
emraan-abbas <imranabbas67@gmail.com>
2022-05-20 00:52:00 +0500

    Basic Road Map

---
+++ src/controllers/blog.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+
+
+// Create Blogs
+export const createBlog = async (req: Request, res: Response) => {
+  try{
+
+  }
+  catch(error){
+    res.status(500).json({
+      message:'Error at CREATE BLOG',
+      error
+    })
+  }
+};
+
+
+// Get BLOG
+export const getBlog = async (req: Request, res: Response) => {
+  try{
+
+  }
+  catch(error){
+    res.status(500).json({
+      message:'Error at Get BLOG',
+      error
+    })
+  }
+};
+
+// Edit BLOG
+export const editBlog = async (req: Request, res: Response) => {
+  try{
+
+  }
+  catch(error){
+    res.status(500).json({
+      message:'Error at Edit BLOG',
+      error
+    })
+  }
+};
+
+
+// Delete BLOG
+export const deleteBlog = async (req: Request, res: Response) => {
+  try{
+
+  }
+  catch(error){
+    res.status(500).json({
+      message:'Error at DELETE BLOG',
+      error
+    })
+  }
+};
