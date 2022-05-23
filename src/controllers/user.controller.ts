e7c8983f736016e8fb48174a0884dd37d240dc7a
emraan-abbas <imranabbas67@gmail.com>
2022-05-23 20:36:58 +0500

    Added JWT and BCRYPT to User Controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from 'express';
 import {Users} from '../entities/user.model';
 import {dbData} from '../app';
-
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 // Create User
 export const createUser = async (req: Request, res: Response) => {
   try{
 
-    const user = dbData.getRepository(Users).create(req.body);
-    const results = await dbData.getRepository(Users).save(user);
-    return res.status(200).json({message:'User Created', results});
+    bcrypt.hash(req.body.password, 8, async(err, hash) => {
+      if(err) {
+        return res.status(400).json({
+          message: 'Error at Bcrypt',
+          err
+        })
+      }
+      else{
+        const user = await dbData.getRepository(Users).create({
+        name:req.body.name,
+        email:req.body.email,
+        password:hash,
+        role:req.body.role,
+        status:req.body.status,
+      });
+      const results = await dbData.getRepository(Users).save(user);
+      return res.status(200).json({message:'User Created', results});
+      }
+    })
     
   }
   catch(error){
@@ -21,6 +38,55 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
+// Login User
+export const logIn = async (req: Request, res: Response) => {
+  try{
+    const user = await dbData.getRepository(Users).findOneBy({
+      email: req.body.email
+    })
+    try{
+      if(user){
+        bcrypt.compare(req.body.password, user.password, async(err, result) => {
+          if(err){
+              res.status(500).json({
+              message:'Error at Bcrypt Compare',
+              err
+            })
+          }
+          else{
+            const token = await jwt.sign(
+              {email: user.email}, 'myKey', {expiresIn: '1h'}
+            );
+            return res.status(200).json({
+              message:'Login Successful',
+              token,
+              result
+            })
+          }
+        })
+      }
+      else{
+        res.status(400).json({
+          message:'User with this email does not exist'
+        })
+      }
+    }
+    catch(error){
+    console.log(error)
+    return res.status(500).json({
+      message:'Error at INNER LOGIN USER FUNCTION',
+      error
+    })
+    }
+  }
+  catch(error){
+    console.log(error)
+    return res.status(500).json({
+      message:'Error at LOGIN USER',
+      error
+    })
+  }
+};
 
 // Get User
 export const getUser = async (req: Request, res: Response) => {
@@ -37,7 +103,6 @@ export const getUser = async (req: Request, res: Response) => {
     })
   }
 };
-
 
 // Edit User
 export const editUser = async (req: Request, res: Response) => {
@@ -56,7 +121,6 @@ export const editUser = async (req: Request, res: Response) => {
     })
   }
 };
-
 
 // Delete User
 export const deleteUser = async (req: Request, res: Response) => {
