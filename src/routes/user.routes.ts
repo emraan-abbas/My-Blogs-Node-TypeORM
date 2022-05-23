5c11a3cbfc45f92362f0c8c1da1c9f3bdcf46fbc
emraan-abbas <imranabbas67@gmail.com>
2022-05-23 20:36:23 +0500

    Login Route Created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createUser, getUser, editUser, deleteUser} from '../controllers/user.controller';
+import {createUser, logIn, getUser, editUser, deleteUser} from '../controllers/user.controller';
 
 const userRouter = express();
 
 userRouter.post('/create', createUser);
-userRouter.get('/login', getUser);
+userRouter.get('/login', logIn);
+userRouter.get('/all', getUser);
 userRouter.put('/edit/:id', editUser);
 userRouter.delete('/delete/:id', deleteUser);
 
